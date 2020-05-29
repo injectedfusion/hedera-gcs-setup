f08a447ce7eae34d3a1a1a4e02e0fe1aed7167ea
Gabriel Rodriguez <gabriel.rodriguez@hedera.com>
2020-05-29 14:44:53 -0400

    Update setup.sh
    This is quick setup script to add to your desktop to speed up deployment.

--- setup.sh
+++ setup.sh
@@ -1,26 +1,61 @@
 #!/bin/bash
 # Setup Script for hedera-gcs-setup
 
-mkdir -pv hedera-mirror-node/vars hedera-mirror-node/roles
-touch hedera-mirror-node/inventory.ini
-cat >> hedera-mirror-node/inventory.ini <<- _EOF_
+mkdir -pv ~/Desktop/hedera-mirror-node/vars ~/Desktop/hedera-mirror-node/roles
+touch ~/Desktop/hedera-mirror-node/inventory.ini
+cat >> ~/Desktop/hedera-mirror-node/inventory.ini <<- _EOF_
 [mirrornode]
 IPADDRESS ansible_ssh_user=USERNAME
 _EOF_
-touch hedera-mirror-node/roles/requirements.yml 
-cat >> hedera-mirror-node/roles/requirements.yml <<- _EOF_
+touch ~/Desktop/hedera-mirror-node/roles/requirements.yml 
+cat >> ~/Desktop/hedera-mirror-node/roles/requirements.yml <<- _EOF_
 ---
 # from github
 - src: https://github.com/injectedfusion/hedera-gcs-setup
 ...
 _EOF_
-touch hedera-mirror-node/vars/project-id.json
-cat >> hedera-mirror-node/vars/project-id.json <<- _EOF_
+touch ~/Desktop/hedera-mirror-node/vars/project-id.json
+cat >> ~/Desktop/hedera-mirror-node/vars/project-id.json <<- _EOF_
 {
   "access_key":"GOOG1E...",
   "secret_key":"H/4..."
 }
 _EOF_
+touch ~/Desktop/hedera-mirror-node/server_build.yml
+cat >> ~/Desktop/hedera-mirror-node/server_build.yml <<- _EOF_
+---
+- hosts: mirrornode
+  vars_files:
+    - ./vars/project-id.json
+  roles:
+    - hedera-gcs-setup
+...
+_EOF_
 
+# Install Ansible Role
+ansible-galaxy install -r ~/Desktop/hedera-mirror-node/roles/requirements.yml --force
 
+touch ~/Desktop/hedera-mirror-node/install_services.sh
+cat >> ~/Desktop/hedera-mirror-node/install_services.sh  <<- _EOF_
+#!/bin/bash
+ansible-playbook -i  ~/Desktop/hedera-mirror-node/inventory.ini  ~/Desktop/hedera-mirror-node/server_build.yml --tags "install"
+_EOF_
 
+touch ~/Desktop/hedera-mirror-node/start_services.sh
+cat >> ~/Desktop/hedera-mirror-node/start_services.sh  <<- _EOF_
+#!/bin/bash
+ansible-playbook -i ~/Desktop/hedera-mirror-node/inventory.ini ~/Desktop/hedera-mirror-node/server_build.yml --tags "start_services"
+_EOF_
+
+touch ~/Desktop/hedera-mirror-node/stop_services.sh
+cat >> ~/Desktop/hedera-mirror-node/stop_services.sh  <<- _EOF_
+#!/bin/bash
+ansible-playbook -i ~/Desktop/hedera-mirror-node/inventory.ini ~/Desktop/hedera-mirror-node/server_build.yml --tags "stop_services"
+_EOF_
+
+chmod +x ~/Desktop/hedera-mirror-node/install_services.sh
+chmod +x ~/Desktop/hedera-mirror-node/start_services.sh
+chmod +x ~/Desktop/hedera-mirror-node/stop_services.sh
+
+echo "Project Directory Established"
+echo "Now go update hedera-mirror-node/inventory.ini and hedera-mirror-node/vars/project-id.json"
